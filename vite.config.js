5170001dbbe774a4c45a4435f424622b998c033b
Aman Raj <146531681+Aman162004@users.noreply.github.com>
2026-03-10 19:13:17 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/clean-street/',
   server: {
     port: 3004,
     host: true
